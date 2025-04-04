ec6f1b7cd0d5da259402505c959c06c159ed644f
Xi Yan <xiyan@meta.com>
2025-04-04 14:26:43 -0700

    Sync updates from stainless branch: main

---
+++ src/resources/chat/index.ts
@@ -0,0 +1,21 @@
+// File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+export {
+  Chat,
+  type CompletionMessage,
+  type CreateChatCompletionRequest,
+  type CreateChatCompletionResponse,
+  type CreateChatCompletionResponseStreamChunk,
+  type MessageImageContentItem,
+  type MessageReasoningContentItem,
+  type MessageTextContentItem,
+  type SystemMessage,
+  type ToolResponseMessage,
+  type UserMessage,
+} from './chat';
+export {
+  Completions,
+  type CompletionCreateParams,
+  type CompletionCreateParamsNonStreaming,
+  type CompletionCreateParamsStreaming,
+} from './completions';
